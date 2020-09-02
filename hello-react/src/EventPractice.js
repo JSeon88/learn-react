b030ec1a9080fbeb0c02be3a72db604f2ff536a7
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-03 02:38:20 +0900

    4.2.3.2 Property Initializer Syntax

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,6 +4,20 @@ class EventPractice extends Component {
   state = {
     message: "",
   };
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  };
+
   render() {
     return (
       <div>
@@ -13,24 +27,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            console.log(e);
-            console.log(e.target.value);
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
