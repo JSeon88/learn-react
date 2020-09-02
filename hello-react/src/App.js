21e7a0296755eafcfa82f2dc58c595c490c45eba
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-03 08:35:34 +0900

    5.4 에러 수정
    App.js에서 함수형으로 써져있어서 this 안 먹힘

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 //import EventPractice from "./EventPractice";
 import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return (
-    <div>
-      <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-      <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+
   //return <EventPractice />;
-};
+}
 
 export default App;
