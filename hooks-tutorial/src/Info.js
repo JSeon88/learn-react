6f971a2cb8c5ff9b4de98ab3828397ab990b81e9
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-07 22:57:58 +0900

    8.2 useEffect

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Info = () => {
-  const [name, setName, useEffect] = useState('');
+  const [name, setName] = useState('');
   const [nickName, setNickName] = useState('');
+  // 리액트 컴포넌트가 랜더링될 때마다 특정 작업을 수행하도록 설정
+  //   useEffect(() => {
+  //     console.log('렌더링이 완료되었습니다');
+  //     console.log({
+  //       name,
+  //       nickName
+  //     });
+  //   });
 
+  // 마운트 될 때만 실행
+  //   useEffect(() => {
+  //     console.log('마운트 될때만 실행됩니다.');
+  //   }, []);
+
+  // 특정 값이 업데이트될 때만 실행하고 싶을 때
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
   const onChangeName = (e) => {
     setName(e.target.value);
   };
