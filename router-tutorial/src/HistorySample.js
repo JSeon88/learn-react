32a256ef12a4ecb0e2e09191fbcb63b2918b479f
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-15 00:11:43 +0900

    13.6.1 history

---
+++ router-tutorial/src/HistorySample.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class HistorySample extends Component {
+  // 뒤로 가기
+  handleGoBack = () => {
+    this.props.history.goBack();
+  };
+
+  // 홈으로 이동
+  handleGoHome = () => {
+    this.props.history.push('/');
+  };
+
+  componentDidMount() {
+    // 이것을 설정하고 나면 페이지에 변화가 생기려고 할 때마다 정말 나갈 것인지를 질문
+    this.unblok = this.props.history.block('정말 떠나실 건가요?');
+  }
+
+  componentWillUnmount() {
+    if (this.unblok) {
+      this.unblok();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleGoBack}>뒤로</button>
+        <button onClick={this.handleGoHome}>홈으로</button>
+      </div>
+    );
+  }
+}
+
+export default HistorySample;
