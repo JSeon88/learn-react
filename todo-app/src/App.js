ebc187c872d5f482c6fb4e2af7b90aed49ed7e72
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-15 09:26:19 +0900

    [plus]todo-app axios 추가, 서버 연동 완료

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,44 +1,53 @@
-import React, {useState, useCallback, useRef} from 'react';
+import React, {useState, useCallback, useRef, useEffect} from 'react';
+import axios from 'axios';
 import ToDoTemplate from './components/ToDoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, done_type: true, subject: 'test1'},
-    {id: 2, done_type: true, subject: 'test2'},
-    {id: 3, done_type: false, subject: 'test3'},
-  ]);
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/todos');
+      setTodos(response.data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  const nextId = useRef(4);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const onInsert = useCallback(
-    (subject) => {
-      const todo = {
-        id: nextId.current,
-        done_type: false,
-        subject,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
+    async (subject) => {
+      await axios({
+        method: 'post',
+        url: 'http://localhost:3001/todos',
+        data: {
+          subject,
+        },
+      });
+      await fetchData();
     },
     [todos],
   );
 
   const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+    async (id) => {
+      await axios.delete(`http://localhost:3001/todos/${id}`);
+      await fetchData();
     },
     [todos],
   );
 
   const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? {...todo, done_type: !todo.done_type} : todo,
-        ),
-      );
+    async (id) => {
+      const todo = todos.find((todo) => todo.id === id);
+      await axios.put(`http://localhost:3001/todos/${id}/done`, {
+        done: !todo.done,
+      });
+      await fetchData();
     },
     [todos],
   );
@@ -49,6 +58,6 @@ function App() {
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </ToDoTemplate>
   );
-}
+};
 
 export default App;
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -8,11 +8,11 @@ import {
 } from 'react-icons/md';
 
 const TodoListItem = ({todo, onRemove, onToggle}) => {
-  const {id, done_type, subject} = todo;
+  const {id, done, subject} = todo;
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', {done_type})} onClick={() => onToggle(id)}>
-        {done_type ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+      <div className={cn('checkbox', {done})} onClick={() => onToggle(id)}>
+        {done ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{subject}</div>
       </div>
       <div className="remove" onClick={() => onRemove(id)}>
