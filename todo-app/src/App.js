26e9cf2f4106cc17587c8c709fee7a67e228d9eb
JSeon88 <j_seon88@naver.com>
2020-09-21 09:03:35 +0900

    issue #1 반영 (#6)
    * issue #1 반영
    
    * pr 처리
    
    Co-authored-by: seonyoung-jeon <seonyoung.jeon@nhntoast.com>

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,63 +1,8 @@
-import React, {useState, useCallback, useRef, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 import ToDoTemplate from './components/ToDoTemplate';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/todos');
-      setTodos(response.data.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const onInsert = useCallback(
-    async (subject) => {
-      await axios({
-        method: 'post',
-        url: 'http://localhost:3001/todos',
-        data: {
-          subject,
-        },
-      });
-      await fetchData();
-    },
-    [todos],
-  );
-
-  const onRemove = useCallback(
-    async (id) => {
-      await axios.delete(`http://localhost:3001/todos/${id}`);
-      await fetchData();
-    },
-    [todos],
-  );
-
-  const onToggle = useCallback(
-    async (id) => {
-      const todo = todos.find((todo) => todo.id === id);
-      await axios.put(`http://localhost:3001/todos/${id}/done`, {
-        done: !todo.done,
-      });
-      await fetchData();
-    },
-    [todos],
-  );
-
-  return (
-    <ToDoTemplate>
-      <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-    </ToDoTemplate>
-  );
+  return <ToDoTemplate />;
 };
 
 export default App;
--- todo-app/src/components/ToDoTemplate.js
+++ todo-app/src/components/ToDoTemplate.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
+import axios from 'axios';
+import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
 import './ToDoTemplate.scss';
 
-const ToDoTemplate = ({children}) => {
+const ToDoTemplate = () => {
+  const [todos, setTodos] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/todos');
+      setTodos(response.data.data);
+    } catch (e) {
+      console.log(e);
+      alert('리스트를 불러 오는데 실패하였습니다.');
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const onInsert = useCallback(
+    async (subject) => {
+      try {
+        await axios({
+          method: 'post',
+          url: 'http://localhost:3001/todos',
+          data: {
+            subject,
+          },
+        });
+        await fetchData();
+      } catch (e) {
+        alert('등록 실패');
+      }
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    async (id) => {
+      try {
+        await axios.delete(`http://localhost:3001/todos/${id}`);
+        await fetchData();
+      } catch (e) {
+        alert('삭제 실패');
+      }
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    async (id) => {
+      const todo = todos.find((todo) => todo.id === id);
+      if (!todo) {
+        return;
+      }
+      try {
+        await axios.put(`http://localhost:3001/todos/${id}/done`, {
+          done: !todo.done,
+        });
+        await fetchData();
+      } catch (e) {
+        alert('토클 처리 실패');
+      }
+    },
+    [todos],
+  );
+
   return (
     <div className="TodoTemplate">
       <div className="app-title">일정 관리</div>
-      <div className="content">{children}</div>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </div>
   );
 };
