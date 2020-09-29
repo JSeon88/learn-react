35bfe64bd9077b7f3b502a54007ddfa5cb9ce998
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-29 16:42:10 +0900

    14.3 newsapi API 키 발급

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=da7ac9ce447d45d1b1fa0c1c0d1f963c',
       );
       setData(response.data);
     } catch (e) {
