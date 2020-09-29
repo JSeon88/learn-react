94092a8fd3bd8135e8e3364a46b11be2c9cc3a73
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-09-29 16:40:36 +0900

    14.2 axios 사용하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import "./App.css";
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return <h1>news-viewer</h1>;
-}
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
+      )}
+    </div>
+  );
+};
 
 export default App;
