3f43ed28a1684b3b2c13e8b36b847b23414b03cb
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-10-04 21:24:36 +0900

    14.4 뉴스 뷰어 UI

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,28 +1,10 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 
+import NewsList from './components/NewsList';
+
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=da7ac9ce447d45d1b1fa0c1c0d1f963c',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
