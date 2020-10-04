b7ce7ad172880f71f1e9cbe5e0a67822557b461e
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-10-05 00:46:53 +0900

    14.8 customHooks : usePromise

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 import NewsItem from './NewsItem';
 
+import usePromise from '../lib/usePromise';
+
 const NewsListBlock = styled.div`
   box-sizing: border-box;
   padding-bottom: 3rem;
@@ -18,24 +20,11 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({category}) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const feachData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=da7ac9ce447d45d1b1fa0c1c0d1f963c`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    feachData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=da7ac9ce447d45d1b1fa0c1c0d1f963c`,
+    );
   }, [category]);
 
   // 대기 중일 때
@@ -43,11 +32,17 @@ const NewsList = ({category}) => {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
-  // 아직 articles 같이 설정되지 않았을 때
-  if (!articles) {
+  // 아직 response 같이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
 
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  const {articles} = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
