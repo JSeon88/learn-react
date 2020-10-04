eef81ec562ef278a78bb52a2a67d33e22660fda1
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-10-05 00:20:50 +0900

    14.7 라우터 적용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,18 +1,10 @@
-import React, {useState, useCallback} from 'react';
+import React from 'react';
+import {Route} from 'react-router-dom';
 
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), [category]);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
