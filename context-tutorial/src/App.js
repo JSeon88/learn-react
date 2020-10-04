71648bbcd09714e93edcb834a7812f3596f87d6f
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-10-05 01:52:51 +0900

    15.3.3 동적 색상 선택 : Context

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
 import { ColorProvider } from "./contexts/Color";
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
