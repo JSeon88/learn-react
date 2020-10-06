0b8bc2a84561e9eb2ee57471f91c63b4106c4d13
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-10-06 20:14:56 +0900

    16.2.4 액션 타입, 액션 생성 함수 정의

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -2,3 +2,14 @@ const divToggle = document.querySelector(".toggle");
 const counter = document.querySelector("h1");
 const btnIncrease = document.querySelector("#increase");
 const btnDecrease = document.querySelector("#decrease");
+
+// 액션 이름 정의
+// 문자열 형태, 주로 대문자. 액션 이름은 유니크
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+// 액션 생성 함수
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ tyep: DECREASE });
