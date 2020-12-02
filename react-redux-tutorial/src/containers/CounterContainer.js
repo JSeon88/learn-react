9176dd41bec073fdf26f2a67376647e9f6fde14f
seonyoung-jeon <seonyoung.jeon@nhntoast.com>
2020-12-02 11:58:56 +0900

    17.5 bindActionCreators 이용

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import Counter from '../components/Counter';
 import {increase, decrease} from '../modules/counter';
@@ -7,16 +8,30 @@ const CounterContainer = ({number, increase, decrease}) => {
   return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number
-});
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  }
-});
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   }
+// });
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number
+  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        increase,
+        decrease
+      },
+      dispatch
+    )
+)(CounterContainer);
