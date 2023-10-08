307a0a33162d51167135776be7ba1b392caad765
sunny <seonyoung.jeon@injeinc.co.kr>
2023-10-09 00:53:49 +0900

    feat: read - todo 검색 추가하기

--- 2023/todo/src/components/TodoList.tsx
+++ 2023/todo/src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Todo } from "../types/todo";
 import TodoItem from "./TodoItem";
 
@@ -6,16 +7,31 @@ type Props = {
 };
 
 const TodoList: React.FC<Props> = ({ todo }) => {
+  const [search, setSearch] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    return search === ""
+      ? todo
+      : todo.filter((it) =>
+          it.content.toLowerCase().includes(search.toLowerCase())
+        );
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
       <input
         className="search_bar"
         type="text"
+        value={search}
+        onChange={onChange}
         placeholder="검색어를 입력하세요"
       ></input>
       <div className="list_wrapper">
-        {todo.map((it) => (
+        {getSearchResult().map((it) => (
           <TodoItem key={it.id} {...it} />
         ))}
       </div>
