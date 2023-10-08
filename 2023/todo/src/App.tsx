42ce581375714b86c8a72e40edf19bb7fee9c67f
sunny <seonyoung.jeon@injeinc.co.kr>
2023-10-09 00:13:58 +0900

    feat: create - 할일 추가하기

--- 2023/todo/src/App.tsx
+++ 2023/todo/src/App.tsx
@@ -1,13 +1,50 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
 
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todo, setTodo] = useState(mockTodo);
+
+  const idRef = useRef(3);
+
+  const onCreate = (content: string) => {
+    const newItem = {
+      id: idRef.current,
+      isDone: false,
+      content,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   );
--- 2023/todo/src/components/TodoEditor.tsx
+++ 2023/todo/src/components/TodoEditor.tsx
@@ -1,12 +1,47 @@
-import React from "react";
+import { useRef, useState } from "react";
 
-const TodoEditor = () => {
+type Props = {
+  onCreate: (content: string) => void;
+};
+
+const TodoEditor: React.FC<Props> = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  // enter키 눌러 추가
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (!content) {
+      inputRef.current?.focus();
+      return;
+    }
+    onCreate(content);
+    // 등록폼 초기화
+    setContent("");
+  };
+
   return (
     <div className="TodoEditor">
       <h4>새로운 Todo 작성하기📝</h4>
       <div className="editor_wrapper">
-        <input type="text" placeholder="새로운 Todo.." />
-        <button>추가</button>
+        <input
+          ref={inputRef}
+          type="text"
+          onKeyDown={onKeyDown}
+          value={content}
+          onChange={onChangeContent}
+          placeholder="새로운 Todo.."
+        />
+        <button onClick={onSubmit}>추가</button>
       </div>
     </div>
   );
