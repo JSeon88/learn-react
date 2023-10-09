d7fdb840ba91062316f9d4d6d25cb6e52e79c73f
sunny <seonyoung.jeon@injeinc.co.kr>
2023-10-09 15:14:14 +0900

    feat: useReducer - useReducer 형식

--- 2023/todo/src/components/TestReducer.tsx
+++ 2023/todo/src/components/TestReducer.tsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+type ActionType = "INCREASE" | "DECREASE";
+type Action = {
+  type: ActionType;
+  data: number;
+};
+
+const reducer = (state: number, action: Action) => {
+  switch (action.type) {
+    case "INCREASE":
+      return state + action.data;
+    case "DECREASE":
+      return state - action.data;
+    default:
+      return state;
+  }
+};
 
 const TestReducer = () => {
-  const [count, setCount] = useState(0);
-
-  const onIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const onDecrement = () => {
-    setCount(count - 1);
-  };
+  const [count, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
       <h4>테스트 컴포넌트</h4>
       <div>{count}</div>
       <div>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
+        <button onClick={() => dispatch({ type: "INCREASE", data: 1 })}>
+          +
+        </button>
+        <button onClick={() => dispatch({ type: "DECREASE", data: 1 })}>
+          -
+        </button>
       </div>
     </div>
   );
