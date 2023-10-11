96f938d45cc4401ab439cc4d3c9f7706a2d378cc
seonyoung-jeon <seonyoung.jeon@injeinc.co.kr>
2023-10-11 13:25:00 +0900

    feat: font, emotion img 추가

--- 2023/diary/src/App.tsx
+++ 2023/diary/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import { getEmotionImgById } from "./util";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>감정 일기장</h1>
+      <img alt="감정1" src={getEmotionImgById("1")}></img>
+    </div>
+  );
 }
 
 export default App;
