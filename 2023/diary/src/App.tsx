af9009b28362820afdaa8f9abc4bf0fb5ca95134
seonyoung-jeon <seonyoung.jeon@injeinc.co.kr>
2023-10-11 13:51:36 +0900

    feat: [react-router] 페이지 라우팅

--- 2023/diary/src/App.tsx
+++ 2023/diary/src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
-import { getEmotionImgById } from "./util";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 
 function App() {
   return (
     <div className="App">
-      <h1>감정 일기장</h1>
-      <img alt="감정1" src={getEmotionImgById("1")}></img>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new" element={<New />}></Route>
+        <Route path="/diary" element={<Diary />}></Route>
+        <Route path="/edit" element={<Edit />}></Route>
+      </Routes>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/edit"}>Edit</Link>
+      </div>
     </div>
   );
 }
--- 2023/diary/src/index.tsx
+++ 2023/diary/src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 브라우저의 주소 변경을 감지 */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
