9f38bdade526921df4550d5966feb4591ebafc0d
seonyoung-jeon <seonyoung.jeon@injeinc.co.kr>
2023-10-11 14:02:43 +0900

    feat: [react-router] 동적 라우팅, useParams, useSearchParams

--- 2023/diary/src/pages/Home.tsx
+++ 2023/diary/src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  // 쿼리 스트링으로 값 불러오기
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("sort"));
   return <div>Home</div>;
 };
 
