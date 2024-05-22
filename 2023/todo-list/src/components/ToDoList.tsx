20d158162e57605aad8d6ff97f4bd38b163b7d64
sunny <seonyoung.jeon@injeinc.co.kr>
2024-05-22 19:14:45 +0900

    feat: category selector 추가

--- 2023/todo-list/src/components/ToDoList.tsx
+++ 2023/todo-list/src/components/ToDoList.tsx
@@ -1,15 +1,23 @@
-import { useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "../atom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atom";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
-  const [todoList, doingList, doneList] = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const toDos = useRecoilValue(toDoSelector);
 
+  const handleCategory = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <>
       <div className="todo">
+        <select value={category} onInput={handleCategory}>
+          <option value={"TO_DO"}>TO_DO</option>
+          <option value={"DOING"}>DOING</option>
+          <option value={"DONE"}>DONE</option>
+        </select>
         <h1>ToDo List</h1>
         <div>
           <CreateToDo />
@@ -18,34 +26,7 @@ const ToDoList = () => {
       <div className="todo-list">
         <ul>
           {toDos.map((todo) => (
-            <ToDo key={todo.id} {...todo} />
-          ))}
-        </ul>
-      </div>
-      <hr />
-      <h1>TO_DO</h1>
-      <div className="todo-list">
-        <ul>
-          {todoList.map((todo) => (
-            <ToDo key={todo.id} {...todo} />
-          ))}
-        </ul>
-      </div>
-      <hr />
-      <h1>DOING</h1>
-      <div className="todo-list">
-        <ul>
-          {doingList.map((todo) => (
-            <ToDo key={todo.id} {...todo} />
-          ))}
-        </ul>
-      </div>
-      <hr />
-      <h1>DONE</h1>
-      <div className="todo-list">
-        <ul>
-          {doneList.map((todo) => (
-            <ToDo key={todo.id} {...todo} />
+            <ToDo {...todo} />
           ))}
         </ul>
       </div>
