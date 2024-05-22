69173543301dc52abc5f78577e677ad43481cd90
sunny <seonyoung.jeon@injeinc.co.kr>
2024-05-22 18:14:32 +0900

    feat: todo category 버튼 변경 추가

--- 2023/todo-list/src/components/ToDo.tsx
+++ 2023/todo-list/src/components/ToDo.tsx
@@ -1,12 +1,36 @@
-import { IToDo } from "../atom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atom";
 
-const ToDo = ({ text }: IToDo) => {
+const ToDo = ({ text, category, id }: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const handleCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const target = event.currentTarget.name as IToDo["category"];
+
+    setToDos((oldToDos) => {
+      return oldToDos.map((toDo) =>
+        toDo.id === id ? { ...toDo, category: target } : toDo
+      );
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={handleCategory}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={handleCategory}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={handleCategory}>
+          Done
+        </button>
+      )}
     </li>
   );
 };
