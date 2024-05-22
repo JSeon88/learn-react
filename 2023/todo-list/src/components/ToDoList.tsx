ea2d87be95f567ac63999dc7521814cb9947ff24
sunny <seonyoung.jeon@injeinc.co.kr>
2024-05-22 18:41:04 +0900

    feat: recoil selector 기본 예제

--- 2023/todo-list/src/components/ToDoList.tsx
+++ 2023/todo-list/src/components/ToDoList.tsx
@@ -1,10 +1,11 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atom";
+import { toDoSelector, toDoState } from "../atom";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 const ToDoList = () => {
   const toDos = useRecoilValue(toDoState);
+  const [todoList, doingList, doneList] = useRecoilValue(toDoSelector);
 
   return (
     <>
@@ -17,6 +18,33 @@ const ToDoList = () => {
       <div className="todo-list">
         <ul>
           {toDos.map((todo) => (
+            <ToDo key={todo.id} {...todo} />
+          ))}
+        </ul>
+      </div>
+      <hr />
+      <h1>TO_DO</h1>
+      <div className="todo-list">
+        <ul>
+          {todoList.map((todo) => (
+            <ToDo key={todo.id} {...todo} />
+          ))}
+        </ul>
+      </div>
+      <hr />
+      <h1>DOING</h1>
+      <div className="todo-list">
+        <ul>
+          {doingList.map((todo) => (
+            <ToDo key={todo.id} {...todo} />
+          ))}
+        </ul>
+      </div>
+      <hr />
+      <h1>DONE</h1>
+      <div className="todo-list">
+        <ul>
+          {doneList.map((todo) => (
             <ToDo key={todo.id} {...todo} />
           ))}
         </ul>
