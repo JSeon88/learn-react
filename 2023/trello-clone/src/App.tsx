e7460a04a32d7808595a8880bd8486aa3a989e9c
sunny <seonyoung.jeon@injeinc.co.kr>
2024-05-23 21:03:04 +0900

    feat: draggable와 dragHandle 추가, react18 이슈 로 인한 라이브러리 교체

--- 2023/trello-clone/src/App.tsx
+++ 2023/trello-clone/src/App.tsx
@@ -1,17 +1,31 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+// react-beautiful-dnd 의 react18 호환을 위한 포크 버전
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
 function App() {
   return (
     <DragDropContext onDragEnd={() => {}}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    One
+                  </li>
+                )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
+                {(provided) => (
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>🔥</span>
+                    Two
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
