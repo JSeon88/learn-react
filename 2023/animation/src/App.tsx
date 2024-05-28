be702e8993fea1eae5307bc1c714a1f7ec6ffda3
sunny <seonyoung.jeon@injeinc.co.kr>
2024-05-28 22:07:13 +0900

    feat: framer-motion 설치 및 기본 사용법 추가

--- 2023/animation/src/App.tsx
+++ 2023/animation/src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BasicAnimation from "./components/BasicAnimation";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,18 +9,10 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <BasicAnimation />
     </Wrapper>
   );
 }
