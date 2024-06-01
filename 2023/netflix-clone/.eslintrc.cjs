4c7b95efd12bd38bd375ab39072751b8230adb6f
sunny <seonyoung.jeon@injeinc.co.kr>
2024-06-01 22:37:49 +0900

    feat: prettier 추가

--- 2023/netflix-clone/.eslintrc.cjs
+++ 2023/netflix-clone/.eslintrc.cjs
@@ -1,18 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es6: true },
   extends: [
+    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/vite-env.d.ts', 'node_modules/', 'dist/'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
--- 2023/netflix-clone/src/assets/styles/GlobalStyle.ts
+++ 2023/netflix-clone/src/assets/styles/GlobalStyle.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -53,7 +53,6 @@ table {
 }
 body {
   font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
   color: black;
 }
 a {
